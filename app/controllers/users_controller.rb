d0a4ea1750523b3a96eaca3ff3e6417a5976fa3c
Abi Noda <a@abinoda.com>
2013-09-02 21:12:12 -0500

    finished dalli

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,13 @@ class UsersController < ApplicationController
   def index
     @last_page = User.count/100
     @number = params[:page].to_i
-    @users = User.by_karma.page(@number)
-    # Rails.cache.read
-    # Rails.cache.write
-    # Rails.cache.fetch
+    puts params
+    @users = Rails.cache.read 'users'
+    if @users
+    else
+      @users = User.by_karma.page(@number)
+      Rails.cache.write('users', @users)
+    end
   end
 
 end
